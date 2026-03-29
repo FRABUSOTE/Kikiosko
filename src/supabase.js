433e67883fea66668714ea30e9389d9e0a2371b8
Franco Bustamante <franco.bustamante.otero@gmail.com>
2026-03-29 11:48:26 -0500

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://rixcntulaffsermowvvj.supabase.co'
-const supabaseKey = 'sb_publishable_fNzxi-zcp_BaLxPUTapDnw_w3qv555K'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpeGNudHVsYWZmc2VybW93dnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2OTU2MTcsImV4cCI6MjA5MDI3MTYxN30.PYuwzVLeTEo1F_Xt57KkrttyScqIFJVHo9BXlY6NMTc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
